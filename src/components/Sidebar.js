defeff202c301b07c38179392687cd621434dcab
3nn50nn <s551w902@gmail.com>
2024-04-05 12:53:35 +0900

    request sidebar new branch

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,27 @@
 import './Sidebar.css';
 import React from 'react';
+import { Avatar } from '@mui/material';
 
-
-function Sidebar.js() {
+function Sidebar() {
 
   return (
     <nav>
 	<ul class="squircles">
 		
   <li class="squircle">
-  
   </li>
 		<li class="divider"></li>
 		<li class="squircle">
+        <Avatar alt="" src=""></Avatar>
 		</li>
 		<li class="squircle">
+        <Avatar alt="" src=""></Avatar>
 		</li>
 		<li class="squircle">
+        <Avatar alt="" src=""></Avatar>
 		</li>
 		<li class="squircle">
+        <Avatar alt="" src=""></Avatar>
 		</li>
 	</ul>
 </nav>
