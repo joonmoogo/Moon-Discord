ccfd209db9f3e6ac088c274ec363de10df4f9ddd
3nn50nn <s551w902@gmail.com>
2024-04-05 12:33:57 +0900

    sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,30 @@
+import './Sidebar.css';
+import React from 'react';
+
+
+function Sidebar.js() {
+
+  return (
+    <nav>
+	<ul class="squircles">
+		
+  <li class="squircle">
+  
+  </li>
+		<li class="divider"></li>
+		<li class="squircle">
+		</li>
+		<li class="squircle">
+		</li>
+		<li class="squircle">
+		</li>
+		<li class="squircle">
+		</li>
+	</ul>
+</nav>
+    
+   
+  );
+}
+
+export default Sidebar;
