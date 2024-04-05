9de74ea89135d69bfd8a64e132b9204e73da6f2c
3nn50nn <s551w902@gmail.com>
2024-04-05 13:04:24 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import './App.css';
 
+
+function App() {
+
+  return (
+    <nav>
+	<ul class="squircles">
+  <li class="squircle"></li>
+		<li class="divider"></li>
+		<li class="squircle"></li>
+		<li class="squircle"></li>
+		<li class="squircle"></li>
+		<li class="squircle"></li>
+	</ul>
+</nav>
+    
+   
+  );
+}
+
+export default App;
