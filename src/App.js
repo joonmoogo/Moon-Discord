969ff155075e5067639ab825627b9f177e6cc9fc
joonmoogo <junemuk10@gmail.com>
2024-05-03 14:25:12 +0900

    add annotation

--- src/App.js
+++ src/App.js
@@ -9,11 +9,18 @@ import ChatUI from './components/chatUI';
 import Sidebar from './components/sidebar';
 import { useEffect } from 'react';
 import socket from './util/socket';
+import useDeviceType from './util/useDeviceType';
+import useUserType from './util/useUserType';
 function App() {
 
-  return (
-    
-    <div className='container' style={containerStyle}>
+  const deviceType = useDeviceType();
+  const userType = useUserType();
+
+
+  
+  return (    
+    <div className='container' style={{ display: 'flex', height: '100vh' }}>
+      {/* 내가 속한 채널 */}
       <div className='left-left' style={{
         backgroundColor: 'red',
         height: '100%',
@@ -22,6 +29,7 @@ function App() {
       }}>
         <Sidebar/>
       </div>
+      {/* 채널 안에 속한 채팅채널, 혹은 음성채널 */}
       <div className='left' style={{
         backgroundColor: 'orange',
         height: '100%',
@@ -31,6 +39,7 @@ function App() {
       >
         <SelectedListItem />
       </div>
+      {/* 채팅 UI */}
       <div className='mid' style={{
         backgroundColor: 'blue',
         height: '100%',
@@ -41,6 +50,7 @@ function App() {
       >
         <ChatUI></ChatUI>
       </div>
+      {/* 채팅방에 속한 친구 목록 */}
       <div className='right' style={{
         backgroundColor: 'green',
         height: '100%',
@@ -52,6 +62,7 @@ function App() {
         <Right></Right>
       </div>
     </div>
+    
   );
 }
 
