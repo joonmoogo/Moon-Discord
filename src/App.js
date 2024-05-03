2c9f10b89da06c70c19d7e3abfdc64fd0421e85a
eldydqkd <122261662+eldydqkd@users.noreply.github.com>
2024-05-03 15:26:58 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,27 +9,20 @@ import ChatUI from './components/chatUI';
 import Sidebar from './components/sidebar';
 import { useEffect } from 'react';
 import socket from './util/socket';
-import useDeviceType from './util/useDeviceType';
-import useUserType from './util/useUserType';
 function App() {
 
-  const deviceType = useDeviceType();
-  const userType = useUserType();
-
-
-  
-  return (    
-    <div className='container' style={{ display: 'flex', height: '100vh' }}>
-      {/* 내가 속한 채널 */}
+  return (
+    
+    <div className='container' style={{display: 'flex', height: '100vh'}}>
       <div className='left-left' style={{
         backgroundColor: 'red',
         height: '100%',
         width: '5%',
         border: '1px solid black',
+        overflowY: 'scroll',
       }}>
         <Sidebar/>
       </div>
-      {/* 채널 안에 속한 채팅채널, 혹은 음성채널 */}
       <div className='left' style={{
         backgroundColor: 'orange',
         height: '100%',
@@ -39,7 +32,6 @@ function App() {
       >
         <SelectedListItem />
       </div>
-      {/* 채팅 UI */}
       <div className='mid' style={{
         backgroundColor: 'blue',
         height: '100%',
@@ -50,19 +42,18 @@ function App() {
       >
         <ChatUI></ChatUI>
       </div>
-      {/* 채팅방에 속한 친구 목록 */}
       <div className='right' style={{
         backgroundColor: 'green',
         height: '100%',
         width: '15%',
         overflow: 'hidden',
         border: '1px solid black',
+        overflowY: 'scroll',
       }}
       >
         <Right></Right>
       </div>
     </div>
-    
   );
 }
 
