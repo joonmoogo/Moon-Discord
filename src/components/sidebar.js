f41681fe0e86e07630e774ec6b80314322808a9f
3nn50nn <s551w902@gmail.com>
2024-05-10 15:29:19 +0900

    사이드바 추가사항

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,29 +1,60 @@
 import './sidebar.css';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Avatar, Box } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import socket from '../util/socket';
 
 function Sidebar() {
+	const [setChannel, setNewchannel] = useState();
 
+
+	function AddChannel() {
+		const channelname = prompt('channel name?');
+		setNewchannel(channelname);
+	
+		
+		socket.emit('channel', (data) => {
+			const newChannel = {
+				channelname: data.channelname,
+			};
+			console.log(newChannel);
+			setChannel(data.name)
+		})
+		
+		let newCh = document.getElementById('newCh');
+		let new_squircle = document.createElement('div');
+		new_squircle.setAttribute('class', 'squircle');
+		new_squircle.innerHTML = channelname;
+		new_squircle.style.marginBottom = "10px";
+		
+		newCh.appendChild(new_squircle);
+		
+	}
+	/*
+	const btn = document.getElementById("btn1");
+	if (btn) {
+	  btn.addEventListener('click', AddChannel);
+	}
+	S	
+	*/
 	return (
 
 		<div class="squircles">
 			<div class="squircle">
 			</div>
-			<div class="divider"></div>
-			<div class="squircle">
-				<Avatar alt="" src=""></Avatar>
+
+			<div class="divider">
 			</div>
-			<div class="squircle">
-				<Avatar alt="" src=""></Avatar>
+
+			<div id = "newCh">
 			</div>
-			<div class="squircle">
-				<Avatar alt="" src=""></Avatar>
-			</div>
-			<div class="squircle">
-				<Avatar alt="" src=""></Avatar>
-			</div>
+
+			<button onClick={AddChannel} id="btn1"  class ="button" ><AddIcon/></button>
+			
+
 		</div>
+
 
 
 	);
